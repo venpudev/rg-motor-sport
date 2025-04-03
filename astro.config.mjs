d1ba492a453152edcb419fa4e47751adcb5b8976
sebahernandez <sebaprogramer@gmail.com>
2025-04-03 18:00:05 -0300

    chore: actualiza configuración para Node.js 20 y Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel";
+import vercel from "@astrojs/vercel/serverless";
 import astroIcon from "astro-icon";
 
 // https://astro.build/config
@@ -17,6 +17,12 @@ export default defineConfig({
     }),
   ],
   output: "server",
-  adapter: vercel(),
+  adapter: vercel({
+    imageService: true,
+    webAnalytics: {
+      enabled: true,
+    },
+    maxDuration: 60,
+  }),
   prefetch: true,
 });
