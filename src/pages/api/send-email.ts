7cad0b52caee832587f441055114bebe6bc30989
sebahernandez <sebaprogramer@gmail.com>
2025-04-02 23:21:55 -0300

    correccion de resend

--- src/pages/api/send-email.ts
+++ src/pages/api/send-email.ts
@@ -2,7 +2,6 @@ import type { APIRoute } from "astro";
 import { Resend } from "resend";
 
 const resend = new Resend(import.meta.env.RESEND_API_KEY);
-const secretKey = import.meta.env.SITE_KEY;
 
 export const POST: APIRoute = async ({ request }) => {
   try {
@@ -19,29 +18,7 @@ export const POST: APIRoute = async ({ request }) => {
       );
     }
 
-    // ✅ Verifica reCAPTCHA v2
-    const verifyURL = `https://www.google.com/recaptcha/api/siteverify`;
-    const response = await fetch(verifyURL, {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams({
-        secret: secretKey,
-        response: recaptcha,
-      }),
-    });
-
-    const verifyData = await response.json();
-    if (!verifyData.success) {
-      return new Response(
-        JSON.stringify({ error: "reCAPTCHA inválido", details: verifyData }),
-        {
-          status: 400,
-          headers: { "Content-Type": "application/json" },
-        }
-      );
-    }
-
-    // ✅ Envío de correo con Resend
+    //Envío de correo con Resend
     const { data: emailData, error } = await resend.emails.send({
       from: "onboarding@resend.dev",
       to: "rgaytan.asesorias@gmail.com",
@@ -61,8 +38,6 @@ export const POST: APIRoute = async ({ request }) => {
         headers: { "Content-Type": "application/json" },
       });
     }
-
-    console.log("Verificación reCAPTCHA:", verifyData);
 
     return new Response(JSON.stringify({ success: true, data: emailData }), {
       status: 200,
